b3e52b3317ac098a27941f88852a3d2f2d67e51e
GoGoNKT <gogonkt@gmail.com>
2009-09-28 15:15:46 +0800

    modified:   engine.f 調用碼表程序解碼，給出對應的字 modified:   idea_book

--- engine.f
+++ engine.f
@@ -1,9 +1,20 @@
 /--engine.f--------
 /--Lune IME egine--
 
-/--parser: 由編碼查出字或詞組
+/--load table.f: IME table file, chs: 直接查字表--
+/--load fuzzy-table.f IME table file for fuzzy, chs: 模糊搜索表，用於實時提示
+/--表結構:
+/--: $table-$encodestring ." font1 font2 font3 ..." ;
+/--for example:
+/--: fuzzy-nihao ." 你好 泥號" ;
+require ./table.f
+require ./fuzzy-table.f
+
+/--parser: 由編碼查出字或詞組----
 : parser ( s1 -- word ) 
-    /--do something
+    /--call $table-$encodestring parse string to font
+	/-- FIX ME: 怎麼 call encodestring 對應的 word
+	table-pns
     ;
 
-/ vim: ft=forth:
+/-- vim: ft=forth: --
