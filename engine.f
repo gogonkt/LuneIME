d0fcead56b6b50d307596d16a01b18f626e55a0b
GoGoNKT <gogonkt@gmail.com>
2009-09-28 20:49:39 +0800

    tweak commnets

--- engine.f
+++ engine.f
@@ -3,10 +3,6 @@
 
 /--load table.f: IME table file, chs: 直接查字表--
 /--load fuzzy-table.f IME table file for fuzzy, chs: 模糊搜索表，用於實時提示
-/--表結構:
-/--: $table-$encodestring ." font1 font2 font3 ..." ;
-/--for example:
-/--: fuzzy-nihao ." 你好 泥號" ;
 require ./table.f
 require ./fuzzy-table.f
 
--- table.f
+++ table.f
@@ -2,6 +2,10 @@
 /-- noseeing encode table
 /-- 嘸啥味 編碼碼表
 /-- This file should auto generate by cin2table
+/--表結構:
+/--: $table-$encodestring ." font1 font2 font3 ..." ;
+/--for example:
+/--: fuzzy-nihao ." 你好 泥號" ;
 
 : table-pns ." 你 倧 傧 您" ;
 /-- vim: ft=forth:
