0810b02e8d89b7ac50e7847b9b4d7b92598fb520
GoGoNKT <gogonkt@gmail.com>
2009-10-04 02:13:35 +0800

    modified:   common.f \\ Add testkey modified:   idea_book

--- common.f
+++ common.f
@@ -1,16 +1,21 @@
-| vim: set ft=reva
+| vim: ft=reva
+
 32 constant bl
+
+: 2tuck 2swap 2over ;
 : ppad " pns 你 ix 我 pal 他 iwn 這 qe 里" pad place ;
+: code-table ppad pad count ;
+
 : findcode search drop ;
 : code> swap drop 1+ /string ;
 | pad count " ix" 2swap 2over .s findcode 2swap .s code> type
-| " pns 你 ix 我 pal 他 iwn 這 qe 里" " ix" findcode .s cr " ix" code>> .s cr 2dup bl /char .s cr nip - type
 : code>> nip 1+ /string ;
-: 1stword 2dup bl /char nip - ;
-: 2tuck 2swap 2over ;
+| " pns 你 ix 我 pal 他 iwn 這 qe 里" " ix" findcode .s cr " ix" code>> .s cr 2dup bl /char .s cr nip - type
+
 : searchcode  2tuck  findcode 2swap code>> ;
+: 1stword 2dup bl /char nip - ;
 | " pns 你 ix 我 pal 他 iwn 這 qe 里" " ix" searchcode 1stword type
-: code-table ppad pad count ;
+
 : code-parser searchcode 1stword ;
 code-table " pal" code-parser type
 bye
