393a9e243bdcf248cd95fe00b2498515aab44dfb
GoGoNKT <gogonkt@gmail.com>
2009-10-10 01:04:11 +0800

    modified:   luneIME.f

--- luneIME.f
+++ luneIME.f
@@ -4,19 +4,43 @@
 | --libs--
 needs ./engine.f
 
+variable buffer
+
+: space? ( char -- flag )
+	dup bl = 
+	;
+
+: cacheit ( char -- )
+	dup buffer c+place
+	;
+
+: decode ( char -- )
+	;
+
 : (luneIME) ( -- )
 repeat
-	ekey dup emit 
+	ekey
+	| -- 回顯
+	dup emit 
+	| -- 把處理輸入和查碼表等
+	space? if decode else cacheit then
 	| -- 如果大寫 Q 退出
 	'Q <> 
 while
-;
+	;
 
 : luneIME ( -- )
+." 按大寫 Q 退出，現有碼表: " cr
+$table type cr
 (luneIME)
-;
+	;
 
-luneIME
-bye
+: clrbuffer " " buffer place ;
+: chkbuffer buffer count type ;
+: test repeat ekey space? if clrbuffer .s ." clr" else cacheit .s ." cached" then .s chkbuffer .s cr 'Q <> while ;
+reset
+test
+| luneIME
+| bye
 
 | vim: ft=reva :
