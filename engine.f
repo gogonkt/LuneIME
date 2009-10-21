10e38645cec65d885fb1191ce154f8f060e8b935
GoGoNKT <gogonkt@gmail.com>
2009-10-21 11:53:07 +0800

    Chang | => () comment

--- engine.f
+++ engine.f
@@ -1,8 +1,10 @@
-| --engine.f--------
-| --Lune IME egine--
+( --engine.f--------
+  --Lune IME egine--
+)
 
-| --load table.f: IME table file, chs: 直接查字表--
-| --load fuzzy-table.f IME table file for fuzzy, chs: 模糊搜索表，用於實時提示
+( --load table.f: IME table file, chs: 直接查字表--
+  --load fuzzy-table.f IME table file for fuzzy, chs: 模糊搜索表，用於實時提示
+)
 needs ./table.f
 
 loadtable
@@ -11,13 +13,13 @@ loadtable
 : getnext ( -- )
 	bl split 3drop
 	bl split drop
-	; | 還要加入是否成功
+	; ( 還要加入是否成功 )
 
 : $table ( -- a n )
 	pad count
 	;
 
-| locate  <index word> in tstr
+( locate  <index word> in tstr )
 : find$ ( a1 n1 a2 n2 -- a3 n3 | " no string!" )
 	search
 	if
@@ -27,12 +29,13 @@ loadtable
 	then
 	;
 
-| --parser: 由編碼查出字或詞組----
+( --parser: 由編碼查出字或詞組---- )
 : parser ( a1 n1 -- a2 n2 )
-	| -- ( s1 -- word ) 
-    | -- call: $encodestring parse string to font
-	| -- chs: 調用碼表程序解碼，給出對應的字
-	| -- FIXME: 怎麼 call encodestring 對應的 word
+	( -- ( s1 -- word ) 
+     -- conversion $encodestring to chinese
+	 -- chs: 調用碼表程序解碼，給出對應的字
+	 -- FIXME: 怎麼 call encodestring 對應的 word
+	)
 	find$
     ;
 
